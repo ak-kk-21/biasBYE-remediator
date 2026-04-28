3b84bc80256b14c52b19bc53ce768f77d0b823ab
Aarya K <152022089+ak-kk-21@users.noreply.github.com>
2026-04-28 23:14:46 +0530

    Fix CORS - allow credentials

--- api.py
+++ api.py
@@ -15,8 +15,10 @@ app = FastAPI(title="BiasBYE Remediator API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
